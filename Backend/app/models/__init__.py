7cab8b512e9bea7c9da49c6bb054b94a9b751878
Patrickson2 <thairupatrickson@gmail.com>
2026-04-23 00:24:53 +0300

    ceated user data

---
+++ Backend/app/models/__init__.py
@@ -0,0 +1,5 @@
+from app.models.user import User, UserRole
+from app.models.field import Field, CropStage
+from app.models.field_update import FieldUpdate
+
+__all__ = ["User", "UserRole", "Field", "CropStage", "FieldUpdate"]
